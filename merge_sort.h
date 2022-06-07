386baa24d3647ae75498d4c867965562a7e6a79a
Yurii Potsiluienko <43655008+yurapots@users.noreply.github.com>
2022-06-07 09:59:18 -0400

    Add files via upload

---
+++ merge_sort.h
@@ -0,0 +1,17 @@
+/*--------------------------------------------------
+Project: Algorithms: Design and Analysis, Part 1
+File:    merge_sort.h
+Author:  Yurii Potsiluienko
+Version: 2022-06-07
+--------------------------------------------------
+*/
+#ifndef MERGE_SORT_H
+#define MERGE_SORT_H
+
+// classic recursive merge sort algorithm
+// time: O(nlogn)
+// space: O(n)
+void merge_sort_recursive(int *array, int length);
+
+
+#endif
